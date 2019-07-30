0cb464b9492136030d4196d7faba8d5cbb72b3c4
Espen Moe-Nilssen <espen@medialog.no>
2019-07-30 13:31:11 +0200

    added installation of favicons

--- src/medialog/dutchestheme/setuphandlers.py
+++ src/medialog/dutchestheme/setuphandlers.py
@@ -3,20 +3,44 @@ from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 from plone import api
 import os
-from plone.app.textfield.value import RichTextValue
+#from plone.app.textfield.value import RichTextValue
+from plone.namedfile.file import NamedBlobImage
 
 def post_install(context):
-    """add front-page script"""
-    #Add front page
-    #portal = api.portal.get()
-    #_create_frontpage(portal)
-
-def install_demo_content(context):
-    """add content script"""
-    #Add demo content
-    #portal = api.portal.get()
-    #_create_content(portal)
+    """add favicons etc"""
+    portal = api.portal.get()
+    _create_content(portal)
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+def _create_content(portal):
+    """Lets create the icons and the user can customize them"""
+    titles = [
+        'favicon.png',
+        'apple-touch-icon-57x57-precomposed.png',
+        'apple-touch-icon-72x72-precomposed.png',
+        'apple-touch-icon-114x114-precomposed.png',
+        'apple-touch-icon-144x144-precomposed.png',
+        'apple-touch-icon-precomposed.png',
+        'apple-touch-icon.png'
+        ]
+
+    for i_image in titles:
+        if not portal.get(i_image, False):
+            icon_image = api.content.create(
+                type='Image',
+                container=portal,
+                id=i_image,
+                title=i_image,
+            )
+            icon_image.image = load_image(i_image)
+
+def load_image(i_image):
+    filename = os.path.join(os.path.dirname(__file__), 'theme',
+                            '{0}'.format(i_image))
+    return NamedBlobImage(
+        data=open(filename, 'r').read(),
+        filename=u'{0}'.format(i_image)
+    )
