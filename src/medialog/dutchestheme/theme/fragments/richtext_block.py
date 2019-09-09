45514080dc0ac95f9438b5a4cf00474577ee2e67
Espen Moe-Nilssen <espen@medialog.no>
2019-09-09 16:21:03 +0200

    richtext block, support for internal url

--- src/medialog/dutchestheme/theme/fragments/richtext_block.py
+++ src/medialog/dutchestheme/theme/fragments/richtext_block.py
@@ -6,4 +6,17 @@ def background_image(self):
     bi = self.data['background_image']
     items = self.context.portal_catalog(UID=bi)
     return items[0].getURL()
-    
+
+
+def get_url(self):
+    url = self.data['url']
+    if url.startswith('${portal_url}'):
+        a, b, c = url.split('/')
+        url = '{0}/{1}'.format(b, c)
+        context_state = self.context.restrictedTraverse(
+            '@@plone_context_state'
+        )
+        url = '/'.join([
+            context_state.canonical_object_url(), url
+        ])
+    return url
