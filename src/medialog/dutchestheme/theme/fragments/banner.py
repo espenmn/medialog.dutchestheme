d24cf81213ca535053dfd5534e1ec8ceeb34ba39
Espen Moe-Nilssen <espen@medialog.no>
2019-10-08 23:42:49 +0200

    Adding multilingual widget for banner fragment

--- src/medialog/dutchestheme/theme/fragments/banner.py
+++ src/medialog/dutchestheme/theme/fragments/banner.py
@@ -1,8 +1,21 @@
 def item_url(self):
     item = load_item(self)
-    return item.getURL()
+    return item
 
 def load_item(self):
     item =  self.data['link']
     items =self.context.portal_catalog(UID=item)
-    return items[0]
+    return items[0].getURL()
+
+def get_url(self):
+    url = self.data['select']
+    if url.startswith('${portal_url}'):
+        spl_url =  (url.split('/'))[1:]
+        url = '/'.join(spl_url)
+        context_state = self.context.restrictedTraverse(
+            '@@plone_context_state'
+        )
+        url = '/'.join([
+            context_state.canonical_object_url(), url
+        ])
+    return url
