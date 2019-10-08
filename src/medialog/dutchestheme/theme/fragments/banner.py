d9f94c64a3b9ec6400a748941846528c1ce8628b
Espen Moe-Nilssen <espen@medialog.no>
2019-10-08 23:50:00 +0200

    adding multilingual widget colorbanner

--- src/medialog/dutchestheme/theme/fragments/banner.py
+++ src/medialog/dutchestheme/theme/fragments/banner.py
@@ -1,8 +1,4 @@
 def item_url(self):
-    item = load_item(self)
-    return item
-
-def load_item(self):
     item =  self.data['link']
     items =self.context.portal_catalog(UID=item)
     return items[0].getURL()
