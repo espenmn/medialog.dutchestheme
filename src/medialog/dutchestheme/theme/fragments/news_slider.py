18cf839c462970dd233734580e540e7d112abc0d
Espen Moe-Nilssen <espen@medialog.no>
2020-01-18 15:16:12 +0700

    setting for sort on news slider

--- src/medialog/dutchestheme/theme/fragments/news_slider.py
+++ src/medialog/dutchestheme/theme/fragments/news_slider.py
@@ -1,9 +1,14 @@
 def get_items(self):
-    limit = self.data['limit']
-    subjects = self.data['subjects']
+    data = self.data
+    limit = data['limit']
+    subjects = data['subjects']
     language = self.context.Language()
+    sorton = 'modified'
+    if 'sort_on' in data.keys():
+        sorton = data['sort_on']
 
     if subjects:
-            return self.context.portal_catalog(portal_type='News Item', Language=language, Subject=subjects, sort_on='modified', sort_order='descending')[:limit]
+            return self.context.portal_catalog(portal_type='News Item', Language=language, Subject=subjects, sort_on=sorton, sort_order='descending')[:limit]
 
-    return self.context.portal_catalog(portal_type='News Item', Language=language, sort_on='modified', sort_order='descending')[:limit]
+    return self.context.portal_catalog(portal_type='News Item', Language=language, sort_on=sorton, sort_order='descending')[:limit]
+
