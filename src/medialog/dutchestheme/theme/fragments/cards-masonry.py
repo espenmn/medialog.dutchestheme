0156f81adf8b506c7b065e8026de68ccc06147d5
Espen Moe-Nilssen <espen@medialog.no>
2020-03-25 12:20:37 +0100

    Add css and settings

--- src/medialog/dutchestheme/theme/fragments/cards-masonry.py
+++ src/medialog/dutchestheme/theme/fragments/cards-masonry.py
@@ -9,3 +9,10 @@ def medium(self):
 def large(self):
     large = self.data['large']
     return 100/large - 2
+
+def sort_on(self):
+    sorton = 'modified'
+    if 'sort_on' in data.keys():
+        sorton = data['sort_on']
+
+    return sorton
