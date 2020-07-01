12855526f4f5d2e3bd59aacc0c7d2443f5f9cab4
Espen Moe-Nilssen <espen@medialog.no>
2020-07-01 15:33:36 +0200

    Added feature

---
+++ src/medialog/dutchestheme/theme/fragments/features2.py
@@ -0,0 +1,30 @@
+def family_css(self):
+    #return css_family_class, like fa, wi
+    iconset = self.iconset()
+    if iconset == 'glyphicon':
+         return 'glyphicon'
+    if iconset == 'mapicon':
+        return 'map-icons'
+    if iconset == 'typicon':
+        return 'typcn'
+    if iconset == 'ionicon':
+        return 'ionicons'
+    if iconset == 'weathericon':
+        return 'wi'
+    if iconset == 'octicon' :
+        return 'octicon'
+    if iconset == 'elusiveicon':
+        return 'el-icon'
+    if iconset == 'medialogfont':
+        return 'medialogfont'
+    if iconset == 'iconpickerfont':
+        return 'iconpickerfont'
+    if iconset == 'lineawsome':
+        return 'linewsome'
+
+    return 'fa'
+
+def iconset(self):
+    """Returns current iconset name This is also used for loading the resources below"""
+    return self.context.portal_registry['medialog.iconpicker.interfaces.IIconPickerSettings.iconset']
+    
