39b5dfb3c153efc21a4092b94e29009733f96133
Espen Moe-Nilssen <espen@medialog.no>
2020-09-24 15:24:36 +0200

    more tries at encode

--- src/medialog/dutchestheme/theme/fragments/banner.py
+++ src/medialog/dutchestheme/theme/fragments/banner.py
@@ -1,10 +1,11 @@
 def item_url(self):
-    item =  self.data['link']
+    item =  self.data['link'].encode('ascii','ignore')
     items =self.context.portal_catalog(UID=item)
-    return items[0].getURL().encode('ascii','ignore')
+    irurl = items[0].getURL()
+    return irurl.encode('ascii','ignore')
 
 def get_url(self):
-    url = self.data['select']
+    url = self.data['select'].encode('ascii','ignore')
     if url.startswith('${portal_url}'):
         spl_url =  (url.split('/'))[1:]
         url = '/'.join(spl_url)
