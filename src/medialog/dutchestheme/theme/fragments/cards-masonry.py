f890e19f4b4767ea248ab36159a94fb24c0e057a
Espen Moe-Nilssen <espen@medialog.no>
2020-11-19 19:35:57 +0100

    Card masonry : date option added

--- src/medialog/dutchestheme/theme/fragments/cards-masonry.py
+++ src/medialog/dutchestheme/theme/fragments/cards-masonry.py
@@ -31,10 +31,4 @@ def editmode(self):
         return self.data['disabled']  == False
     return False
 
-def editmode(self):
-    form = self.request.form
-    if  '_layouteditor' in form:
-        return True
-    if  'disabled' in self.data:
-        return self.data['disabled']  == False
-    return True
+ 
