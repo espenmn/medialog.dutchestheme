c0749336c02b630bcff60e3152a86e1cea50901e
Espen Moe-Nilssen <espen@medialog.no>
2020-12-21 13:22:53 +0100

    blue updates

--- src/medialog/dutchestheme/theme/fragments/lr_block.py
+++ src/medialog/dutchestheme/theme/fragments/lr_block.py
@@ -15,3 +15,27 @@ def editmode(self):
     if  'disabled' in self.data:
         return self.data['disabled']  == False
     return True
+
+def get_theurl(self):
+    link =  self.data['link']
+    if link:
+        item =  link.encode('ascii','ignore')
+        items =self.context.portal_catalog(UID=item)
+        irurl = items[0].getURL()
+        return irurl.encode('ascii','ignore')
+
+    url = self.data['select']
+
+    if url:
+        if url.startswith('${portal_url}'):
+            spl_url =  (url.split('/'))[1:]
+            url = '/'.join(spl_url)
+            context_state = self.context.restrictedTraverse(
+                '@@plone_context_state'
+            )
+            url = '/'.join([
+                context_state.canonical_object_url(), url
+            ])
+        return url
+
+    return '#'
