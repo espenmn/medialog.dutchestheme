64be728bde0a40d23d007f2197c363e2b2f99be8
Espen Moe-Nilssen <espen@medialog.no>
2021-04-26 16:34:15 +0200

    Update folderlisting fragment (card)

--- src/medialog/dutchestheme/theme/fragments/folderlisting.py
+++ src/medialog/dutchestheme/theme/fragments/folderlisting.py
@@ -1,14 +1,17 @@
 def get_items(self):
     language = self.context.Language()
     linked = self.data['linked_folder']
-    folder_path = '/'.join(context.getPhysicalPath())
-    content_type = self.data['content_type']
-    sort_on = self.data['sort_on']
-    sort_order = str(self.data['sort_order'])
     if linked:
-        folder_path = self.get_path()
-    folder_path.encode('ascii','ignore')
-    return self.context.portal_catalog(portal_type=content_type, path={'query': folder_path,}, Language=language, sort_on=sort_on, sort_order=sort_order)
+        folder = self.context.portal_catalog(UID=linked)
+        mappe =  folder[0].getObject()
+        folder_path = '/'.join(mappe.getPhysicalPath())
+        if mappe.portal_type != 'Collection':
+            content_type = self.data['content_type']
+            sort_on = self.data['sort_on']
+            sort_order = str(self.data['sort_order'])
+            folder_path.encode('ascii','ignore')
+
+            return self.context.portal_catalog(portal_type=content_type, path={'query': folder_path,}, Language=language, sort_on=sort_on, sort_order=sort_order)
 
 def get_path(self):
     linked = self.data['linked_folder']
@@ -23,3 +26,10 @@ def editmode(self):
     if  'disabled' in self.data:
         return self.data['disabled']  == False
     return False
+
+def list_items(self):
+    linked = self.data['linked_folder']
+    if linked:
+        folder = self.context.portal_catalog(UID=linked)
+        if folder[0].portal_type=='Collection':
+            return folder[0].getObject()
